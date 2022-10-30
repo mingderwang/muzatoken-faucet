bee9874f0b2c5dacdb6f90e7754e6fd695dcfced
Ming-der Wang <mingderwang@operamail.com>
2022-10-30 12:57:04 +0800

    add MuzaToken2.sol as a ERC1155 from https://wizard.openzeppelin.com/\#erc1155

--- scripts/deploy_mytoken.js
+++ scripts/deploy_mytoken.js
@@ -4,11 +4,9 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   const MuzaToken2 = await ethers.getContractFactory("MuzaToken2");
 
-  const mt = await upgrades.deployProxy(
-    MuzaToken2,
-    ["0x611586817af4e77e23951524Df7D8fcEeCf5B3C8"],
-    { initializer: "initialize" }
-  );
+  const mt = await upgrades.deployProxy(MuzaToken2, [], {
+    initializer: "initialize",
+  });
 
   await mt.deployed();
   console.log("MyToken deployed to:", mt.address);
